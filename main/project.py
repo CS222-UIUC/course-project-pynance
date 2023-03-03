cf249d639c66b3750a41b7f402ac7cecbfa728f4
Aryan Malhotra <aryanm8@illinois.edu>
2023-03-02 23:06:38 -0600

    Reads from existing file, saves company info

--- main/project.py
+++ main/project.py
@@ -2,21 +2,27 @@
 import os
 from pathlib import Path
 import yfinance as yf
-#import pandas as pd
+import pandas as pd
 
 def get_company_data(stock_name):
     """Fetches Company Stock Data and Stores it as CSV file"""
     if exists(stock_name):
         #read File
-        a_ = 2   #pass linter temporary checks
-        a_ += 1
+        stock_data = pd.read_csv('data/' + str(stock_name) + '.csv')
+        return stock_data
     else:
         stock_data = yf.download(tickers=stock_name, period='1d', interval='1m')
-        print(stock_data)
+        #print(stock_data)
         #Write it into a file
         filepath = Path('data/' + str(stock_name) + '.csv')
         filepath.parent.mkdir(parents=True, exist_ok=True)
         stock_data.to_csv(filepath)
+
+        #Write descriptions
+        filepath = Path('data/' + str(stock_name) + '.txt')
+        filepath.parent.mkdir(parents=True, exist_ok=True)
+        #stock_data.to_csv(filepath)  FIGURE OUT HOW TO WRITE STOCK_DATA.INFO TO A TXT FILE
+        return stock_data
 
 
 def exists(stock_name):
@@ -28,4 +34,7 @@ def exists(stock_name):
             return True
     return False
 
-#get_company_data("GOOG")
+def main():
+    """Main Function"""
+
+    return 0
