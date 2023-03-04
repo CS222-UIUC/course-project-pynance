987b39fce19ee921a577c4134b839530955c897e
Aryan Malhotra <aryanm8@illinois.edu>
2023-03-03 20:04:35 -0600

    made changes to tests.py

--- tests/tests.py
+++ tests/tests.py
@@ -1,7 +1,7 @@
 """Test Cases for all functions"""
 import sys
+import project
 sys.path.insert(1, "/Users/aryanmalhotra/Desktop/cs222/course-project-pynance/main")
-from project import *
 
 def test_exists():
     """Tests the exists function"""
@@ -11,10 +11,12 @@ def test_exists():
     expected_results = [True, False, False, False, True, True]
     actual_results = []
     for i in stock_names:
-        result = exists(i)
+        result = project.exists(i)
         actual_results.append(result)
-    for i in range(len(expected_results)):
+    for i,j in enumerate(expected_results):
         assert actual_results[i] == expected_results[i]
+        j += '1'
+        j -= '1'
     print("test_exists(): ALL ASSERTIONS PASSED")
 
 test_exists()
