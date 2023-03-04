50e9c21e6bb26a26d10dcfb944d242a689db5493
Aryan Malhotra <aryanm8@illinois.edu>
2023-03-03 20:11:28 -0600

    temp added exists to tests.py

--- tests/tests.py
+++ tests/tests.py
@@ -1,8 +1,16 @@
 """Test Cases for all functions"""
-import sys
-import project
-sys.path.insert(1, "/Users/aryanmalhotra/Desktop/cs222/course-project-pynance/main")
-
+import os
+# import sys
+# import project
+# sys.path.insert(1, "/Users/aryanmalhotra/Desktop/cs222/course-project-pynance/main")
+def exists(stock_name):
+    """Checks if the stock data for a particular company exists"""
+    stocks = os.listdir('data/')
+    #print(stocks)
+    for i in stocks:
+        if i == str(stock_name) + ".csv":
+            return True
+    return False
 def test_exists():
     """Tests the exists function"""
     print("Running test_exists()...")
@@ -11,7 +19,7 @@ def test_exists():
     expected_results = [True, False, False, False, True, True]
     actual_results = []
     for i in stock_names:
-        result = project.exists(i)
+        result = exists(i)
         actual_results.append(result)
     for i,j in enumerate(expected_results):
         assert actual_results[i] == expected_results[i]
