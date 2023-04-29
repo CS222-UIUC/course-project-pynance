c6d8da43cc00f2b6140c3100507ccb63e6121729
MalhotraAryan <aryanm8@illinois.edu>
2023-04-29 15:15:09 -0500

    GUI

---
+++ project/GUI.py
@@ -0,0 +1,129 @@
+import dearpygui
+import dearpygui.dearpygui as dpg
+import project
+from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
+import mplfinance as mpf
+import yfinance as yf
+import datetime
+import matplotlib.pyplot as plt
+
+dpg.create_context()
+
+
+def show_data(sender, data):
+    inputvalue = dpg.get_value("ticker1")
+    period = dpg.get_value("period1")
+    interval = dpg.get_value("interval1")
+    dpg.delete_item("Primary Window")
+    with dpg.window(tag = "Data"):
+        dpg.set_primary_window("Data", True)
+        comdata = project.get_company_data(inputvalue,period,interval)
+        dpg.add_text(comdata)
+        dpg.add_button(tag = "return3",label = "Return", callback = returnmenu)
+
+def show_vis(sender, data):
+    inputvalue = dpg.get_value("ticker3")
+    period = dpg.get_value("period3")
+    interval = dpg.get_value("interval3")
+    hist = project.get_company_data(inputvalue,period,interval)
+
+
+    date =  [float(10 + i*(600/len(hist))) for i in range(len(hist))]
+    opens = hist["Open"].to_list()
+    open = [float(x) for x in opens]
+    closes = hist["Close"].to_list()
+    close = [float(x) for x in closes]
+    lows = hist["Low"].to_list()
+    low = [float(x) for x in lows]
+    highs = hist["High"].to_list()
+    high = [float(x) for x in highs]
+
+    dpg.delete_item("Primary Window")
+    with dpg.window(tag = "Visualization"):
+        dpg.set_primary_window("Visualization", True)
+        with dpg.plot(label="CandlePlot", width=-1, height=-1) as plot_id:
+            dpg.add_plot_legend()
+            dpg.add_plot_axis(dpg.mvXAxis, tag="Dates", time=True)
+            
+            dpg.add_plot_axis(dpg.mvYAxis, tag="Prices")
+            
+            dpg.add_candle_series(dates=date,opens=open, highs=high, lows=low, closes=close, label="Candlesticks",
+                              parent=dpg.last_item())
+
+def show_summ(sender, data):
+    inputvalue = dpg.get_value("ticker2")
+    dpg.delete_item("Primary Window")
+    with dpg.window(tag = "Summary"):
+        dpg.set_primary_window("Summary", True)
+        summary = project.get_company_summary(inputvalue)
+        dpg.add_text(summary)
+        dpg.add_button(tag = "return2",label = "Return", callback = returnmenu)
+
+
+def mainmenu():
+    with dpg.window(tag="Primary Window"):
+        dpg.set_primary_window("Primary Window", True)
+        dpg.add_text("Welcome to Pynance")
+        with dpg.tab_bar():
+            with dpg.tab(label="Data"):
+                dpg.add_text("Enter the Ticker, Period, and Interval for the stock below")
+                dpg.add_input_text(tag = "ticker1")
+                dpg.add_input_text(tag = "period1")
+                dpg.add_input_text(tag = "interval1")
+                dpg.add_button(tag = "submit2", label = "Submit", callback = show_data)
+
+            with dpg.tab(label="Summary"):
+                dpg.add_text("Enter the ticker for the stock below")
+                dpg.add_input_text(tag = "ticker2")
+                dpg.add_button(tag = "submit1", label = "Submit", callback = show_summ)
+
+            with dpg.tab(label="Visualization"):
+                dpg.add_text("Enter the Ticker, Period, and Interval for the stock below")
+                dpg.add_input_text(tag = "ticker3")
+                dpg.add_input_text(tag = "period3")
+                dpg.add_input_text(tag = "interval3")
+                dpg.add_button(tag = "submit3", label = "Submit", callback = show_vis)
+            with dpg.tab(label="Prediction"):
+                dpg.add_text("Enter the Ticker, Period, and Interval for the stock below")
+
+
+
+
+def returnmenu(sender):
+    dpg.delete_item("Data")
+    dpg.delete_item("Visualization")
+    dpg.delete_item("Summary")
+    with dpg.window(tag="Primary Window"):
+        dpg.set_primary_window("Primary Window", True)
+        dpg.add_text("Welcome to Pynance")
+        with dpg.tab_bar():
+            with dpg.tab(label="Data"):
+                dpg.add_text("Enter the Ticker, Period, and Interval for the stock below")
+                dpg.add_input_text(tag = "ticker1")
+                dpg.add_input_text(tag = "period1")
+                dpg.add_input_text(tag = "interval1")
+                dpg.add_button(tag = "submit2", label = "Submit", callback = show_data)
+
+            with dpg.tab(label="Summary"):
+                dpg.add_text("Enter the ticker for the stock below")
+                dpg.add_input_text(tag = "ticker2")
+                dpg.add_button(tag = "submit1", label = "Submit", callback = show_summ)
+
+            with dpg.tab(label="Visualization"):
+                dpg.add_text("Enter the Ticker, Period, and Interval for the stock below")
+                dpg.add_input_text(tag = "ticker3")
+                dpg.add_input_text(tag = "period3")
+                dpg.add_input_text(tag = "interval3")
+                dpg.add_button(tag = "submit3", label = "Submit", callback = show_vis)
+            with dpg.tab(label="Prediction"):
+                dpg.add_text("Enter the Ticker, Period, and Interval for the stock below")
+
+
+mainmenu()
+
+dpg.create_viewport(title='PyNance', width=900, height=900)
+dpg.setup_dearpygui()
+dpg.show_viewport()
+dpg.set_primary_window("Primary Window", True)
+dpg.start_dearpygui()
+dpg.destroy_context()
--- project/project.py
+++ project/project.py
@@ -17,7 +17,7 @@ from sklearn.linear_model import LinearRegression
 def exists_data(stock_name, custom_period, custom_interval):
     """Checks if the stock data for a particular company exists"""
 
-    stocks = os.listdir('data/')
+    stocks = os.listdir('../data/')
     #print(stocks)
     for i in stocks:
         if i == str(stock_name) + '_' + custom_period + '_' + custom_interval + ".csv":
@@ -29,7 +29,7 @@ def get_company_data(stock_name, custom_period, custom_interval):
 
     if exists_data(stock_name, custom_period, custom_interval):
         #read File
-        stock_data = pd.read_csv('data/' + str(stock_name) + '_' +
+        stock_data = pd.read_csv('../data/' + str(stock_name) + '_' +
                                 custom_period + '_' + custom_interval + '.csv')
         return stock_data
     stock_data = yf.download(tickers=stock_name, period=custom_period, interval=custom_interval)
@@ -44,56 +44,56 @@ def get_company_data(stock_name, custom_period, custom_interval):
 def exists_info(stock_name):
     """Checks if the stock info for a particular company exists"""
 
-    stocks = os.listdir('info/')
+    stocks = os.listdir('../info/')
     for i in stocks:
         if i == str(stock_name) + ".txt":
             return True
     return False
-if __name__ == "__main__":
-    def get_company_summary(stock_name):
-        """Fetches Company Summary and Stores it as txt file
-        Returns the company summary string"""
 
-        if exists_info(stock_name):
-            #read info
-            with open('info/' + str(stock_name) + '.txt', "r", encoding="utf-8") as file:
-                stock_information = file.read()
-                return stock_information
-        else:
-            #FinnHub API key and website url
-            api_key = ""
-            with open('config.json', 'r', encoding="utf-8") as api_file:
-                config = json.load(api_file)
-                api_key = config['api_key']
-            url = f"https://finnhub.io/api/v1/stock/profile2?symbol={stock_name}&token={api_key}"
+def get_company_summary(stock_name):
+    """Fetches Company Summary and Stores it as txt file
+    Returns the company summary string"""
 
-            #Used finnhub.io to retreive more information about the stock
-            response = requests.get(url, timeout=60)
-
-            #Parse the data in json and store it
-            data = response.json()
-            stock_information = ""
-            for key in data:
-                stock_information += str(key) + ": " + str(data[key]) + "\n"
-            # Web Scraping
-            # #Get the website url of the company
-            # url = data['weburl']
-
-            # #Used the Company Website to get summary
-            # response = requests.get(url)
-            # print("WEB URL", url)
-
-            # #Web Scraping using bs4 library
-            # #Web Scrape the company's website get summary
-            # soup = BeautifulSoup(response.content, 'html.parser')
-            # stock_information = soup.find('meta', attrs={'name': 'description'})['content']
-            # stock = yf.Ticker(stock_name)
-            # stock_information = stock.info
-
-            #write data
-            with open('info/' + str(stock_name) + '.txt', "w", encoding="utf-8") as file:
-                file.write(stock_information)
+    if exists_info(stock_name):
+        #read info
+        with open('../info/' + str(stock_name) + '.txt', "r", encoding="utf-8") as file:
+            stock_information = file.read()
             return stock_information
+    else:
+        #FinnHub API key and website url
+        api_key = ""
+        with open('config.json', 'r', encoding="utf-8") as api_file:
+            config = json.load(api_file)
+            api_key = config['api_key']
+        url = f"https://finnhub.io/api/v1/stock/profile2?symbol={stock_name}&token={api_key}"
+
+        #Used finnhub.io to retreive more information about the stock
+        response = requests.get(url, timeout=60)
+
+        #Parse the data in json and store it
+        data = response.json()
+        stock_information = ""
+        for key in data:
+            stock_information += str(key) + ": " + str(data[key]) + "\n"
+        # Web Scraping
+        # #Get the website url of the company
+        # url = data['weburl']
+
+        # #Used the Company Website to get summary
+        # response = requests.get(url)
+        # print("WEB URL", url)
+
+        # #Web Scraping using bs4 library
+        # #Web Scrape the company's website get summary
+        # soup = BeautifulSoup(response.content, 'html.parser')
+        # stock_information = soup.find('meta', attrs={'name': 'description'})['content']
+        # stock = yf.Ticker(stock_name)
+        # stock_information = stock.info
+
+        #write data
+        with open('info/' + str(stock_name) + '.txt', "w", encoding="utf-8") as file:
+            file.write(stock_information)
+        return stock_information
 
 ### Visualization Component ###
 if __name__ == "__main__":
@@ -127,40 +127,40 @@ def run_linear_regression(stock_data):
     regression.fit(train_x, train_y)
     return (regression, test_x, test_y)
 
-if __name__ == "__main__":
-    def get_stats(model, test_x, test_y):
-        """Provides Statistics of the Linear Model"""
-        print("regression coefficient",model.coef_)
-        print("regression intercept",model.intercept_)
-        # the coefficient of determination R²
-        regression_confidence = model.score(test_x, test_y)
-        print("linear regression confidence: ", regression_confidence)
 
-        predicted=model.predict(test_x)
-        dfr=pd.DataFrame({'Actual_Price':test_y, 'Predicted_Price':predicted})
-        print(dfr.head(10))
+def get_stats(model, test_x, test_y):
+    """Provides Statistics of the Linear Model"""
+    print("regression coefficient",model.coef_)
+    print("regression intercept",model.intercept_)
+    # the coefficient of determination R²
+    regression_confidence = model.score(test_x, test_y)
+    print("linear regression confidence: ", regression_confidence)
 
-        print('Mean Absolute Error (MAE):', metrics.mean_absolute_error(test_y, predicted))
+    predicted=model.predict(test_x)
+    dfr=pd.DataFrame({'Actual_Price':test_y, 'Predicted_Price':predicted})
+    print(dfr.head(10))
 
-        x_2 = dfr.Actual_Price.mean()
-        y_2 = dfr.Predicted_Price.mean()
-        print("The accuracy of the model is " , x_2/y_2*100)
+    print('Mean Absolute Error (MAE):', metrics.mean_absolute_error(test_y, predicted))
+
+    x_2 = dfr.Actual_Price.mean()
+    y_2 = dfr.Predicted_Price.mean()
+    print("The accuracy of the model is " , x_2/y_2*100)
 
     # Visualization
-if __name__ == "__main__":
-    def get_plots(model, test_x, test_y):
-        """Visualizes the Model"""
-        predicted=model.predict(test_x)
-        dfr=pd.DataFrame({'Actual_Price':test_y, 'Predicted_Price':predicted})
-        plt.scatter(dfr.Actual_Price, dfr.Predicted_Price,  color='Darkblue')
-        plt.xlabel("Actual Price")
-        plt.ylabel("Predicted Price")
-        plt.show()
 
-        plt.plot(dfr.Actual_Price, color='black')
-        plt.plot(dfr.Predicted_Price, color='lightblue')
-        plt.title("Nio prediction chart")
-        plt.show()
+def get_plots(model, test_x, test_y):
+    """Visualizes the Model"""
+    predicted=model.predict(test_x)
+    dfr=pd.DataFrame({'Actual_Price':test_y, 'Predicted_Price':predicted})
+    plt.scatter(dfr.Actual_Price, dfr.Predicted_Price,  color='Darkblue')
+    plt.xlabel("Actual Price")
+    plt.ylabel("Predicted Price")
+    plt.show()
+
+    plt.plot(dfr.Actual_Price, color='black')
+    plt.plot(dfr.Predicted_Price, color='lightblue')
+    plt.title("Nio prediction chart")
+    plt.show()
 
 def get_predicted_price(stock_name, predict_open,
                         predict_high, predict_low, predict_vol):
@@ -181,48 +181,48 @@ if __name__ == "__main__":
         """Main Function"""
         print("How many stocks do you want data on?")
         print("Enter a non-negative integer here: ")
-        # n_1 = int(input())
-        # while n_1 > 0:
-        #     print("------Which stock would you like data on?------")
-        #     stock_name = str(input("Please enter it's symbol: "))
-        #     period = str(input("""Please enter the period over which you want the data
-        #                         (1d -> 1 day; 1m -> 1 month; 1y -> 1 year): """))
-        #     interval = str(input("""Please enter the interval between which you want the data
-            # (1d -> 1 day; 1m -> 1 month; 1y -> 1 year): """))
-            # data = get_company_summary(stock_name)
-            # print(data)
-            # stock_data = get_company_data(stock_name, period, interval)
-            # model, test_x, test_y = run_linear_regression(stock_data)
-            # want_stats = input("Would you like the statistics of the model? (y/n)")
-            # if want_stats == 'y':
-            #     get_stats(model, test_x, test_y)
-            # want_plots = input("Would you like the plots of the data? (y/n)")
-            # if want_plots == 'y':
-            #     get_plots(model, test_x, test_y)
-            # predict_open = int(input("Enter the opening price of the stock: "))
-            # predict_high = int(input("Enter the high of the stock: "))
-            # predict_low = int(input("Enter the low of the stock: "))
-            # predict_vol = int(input("Enter the volume the stock: "))
-            # predict_df = pd.DataFrame({"Open": [predict_open], "High": [predict_high],
-            #                 "Low": [predict_low], "Volume": [predict_vol]})
-            # print("The predicted closing price of", stock_name, "is:",
-            #     model.predict(predict_df)[0])
-            # print("*************")
-            # print("Thank you for using this app")
-            # print("*************")
-            # ### Visualization Component ###
-            # n_1 -= 1
-            # # period = '1d'
-            # period = str(input('Enter a period (1d, 1m, 1y): '))
-            # # interval = '5m'
-            # interval = str(input('Enter an interval (1d, 1m, 1y): '))
+        n_1 = int(input())
+        while n_1 > 0:
+            print("------Which stock would you like data on?------")
+            stock_name = str(input("Please enter it's symbol: "))
+            period = str(input("""Please enter the period over which you want the data
+                                (1d -> 1 day; 1m -> 1 month; 1y -> 1 year): """))
+            interval = str(input("""Please enter the interval between which you want the data
+            (1d -> 1 day; 1m -> 1 month; 1y -> 1 year): """))
+            data = get_company_summary(stock_name)
+            print(data)
+            stock_data = get_company_data(stock_name, period, interval)
+            model, test_x, test_y = run_linear_regression(stock_data)
+            want_stats = input("Would you like the statistics of the model? (y/n)")
+            if want_stats == 'y':
+                get_stats(model, test_x, test_y)
+            want_plots = input("Would you like the plots of the data? (y/n)")
+            if want_plots == 'y':
+                get_plots(model, test_x, test_y)
+            predict_open = int(input("Enter the opening price of the stock: "))
+            predict_high = int(input("Enter the high of the stock: "))
+            predict_low = int(input("Enter the low of the stock: "))
+            predict_vol = int(input("Enter the volume the stock: "))
+            predict_df = pd.DataFrame({"Open": [predict_open], "High": [predict_high],
+                            "Low": [predict_low], "Volume": [predict_vol]})
+            print("The predicted closing price of", stock_name, "is:",
+                model.predict(predict_df)[0])
+            print("*************")
+            print("Thank you for using this app")
+            print("*************")
+            ### Visualization Component ###
+            n_1 -= 1
+            # period = '1d'
+            period = str(input('Enter a period (1d, 1m, 1y): '))
+            # interval = '5m'
+            interval = str(input('Enter an interval (1d, 1m, 1y): '))
 
-            # # stocks = ['MSFT', 'BTC-USD']
+            # stocks = ['MSFT', 'BTC-USD']
             # stocks = input('Enter stock name
             # abbreviation separated by commas (AAPL, GOOG, AMZN): ')
-            # stocks = stocks.replace(' ', '').split(',')
+            # # stocks = stocks.replace(' ', '').split(',')
 
-            # plot_stocks_df(stocks, period, interval)
+            # # plot_stocks_df(stocks, period, interval)
             # keyput = str(input("Would You like data
             # on another stock?[press y if yes, otherwise no]"))
             # if keyput != 'y':
